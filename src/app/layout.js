0d5a3b926a91f61b65e33bb46ecdf5091634f139
Twane70 <tonduantoine@gmail.com>
2024-03-15 09:59:11 +0100

    opengraph

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,6 +9,16 @@ export const metadata = {
   description: 'Web Designer - Développeur - Data Scientist',
 }
 
+export const metadata = {
+  title: 'Antoine Tondu | Portfolio',
+  description: 'Web Designer - Développeur - Data Scientist',
+  keywords: ["Photosphere", "BG", "Hello Bong", "Haricot rouge"],
+  openGraph: {
+    images: 'https://www.eff.org/files/banner_library/patent-troll-3b.png',
+  },
+};
+
+
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
