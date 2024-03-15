a188b0110005b61b8a0e315cf55104402e9f2bb2
Twane70 <tonduantoine@gmail.com>
2024-03-15 10:00:13 +0100

    delete pg

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,11 +7,6 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Antoine Tondu | Portfolio',
   description: 'Web Designer - Développeur - Data Scientist',
-}
-
-export const metadata = {
-  title: 'Antoine Tondu | Portfolio',
-  description: 'Web Designer - Développeur - Data Scientist',
   keywords: ["Photosphere", "BG", "Hello Bong", "Haricot rouge"],
   openGraph: {
     images: 'https://www.eff.org/files/banner_library/patent-troll-3b.png',
