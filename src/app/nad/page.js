98bd93fa854df1cb3f6ccddfb4d1bfd074475215
Twane70 <tonduantoine@gmail.com>
2024-08-31 11:33:09 -0400

    feat: add personal ressources page

---
+++ src/app/nad/page.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import styles from './style.module.scss';
+
+export default function NewPage() {
+  return (
+    <div className={styles.description}>
+      <p>Art & Histoire de l'Image Numérique</p>
+      <Link href="https://docs.google.com/document/d/1BRYuY5FE_rsGDGbONMvUdoMNI6XNqM9jinJeqT_DmZU/edit?usp=sharing">
+        Document
+      </Link>
+    </div>
+  )
+}
