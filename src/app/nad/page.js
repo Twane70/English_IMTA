a00121df8fd7b78c5fa8c6209bdb16b6b4884420
Twane70 <tonduantoine@gmail.com>
2024-08-31 11:39:08 -0400

    fix: apostrophe

--- src/app/nad/page.js
+++ src/app/nad/page.js
@@ -4,7 +4,7 @@ import styles from './style.module.scss';
 export default function NewPage() {
   return (
     <div className={styles.description}>
-      <p>Art & Histoire de l'Image Numérique</p>
+      <p>{"Art & Histoire de l'Image Numérique"}</p>
       <Link href="https://docs.google.com/document/d/1BRYuY5FE_rsGDGbONMvUdoMNI6XNqM9jinJeqT_DmZU/edit?usp=sharing">
         Document
       </Link>
